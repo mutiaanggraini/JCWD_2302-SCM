233e24e47c9a8383ec815ba0935d163441f82432
mutiaanggraini <mutiaanggrainiid@gmail.com>
2023-01-23 11:40:48 +0700

    create new variable

---
+++ git-commands.js
@@ -0,0 +1,9 @@
+// 1. git init (inside project directory) (warning! first initiate  only)
+// 2. git add index.js / git add . kalau mau semuanya
+// 3. git commit -m "Create variable inside index.js"
+// 4. git log
+// 5. git remote add origin https://github.com/mutiaanggraini/JCWD_2302-SCM.git (warning! first initiate  only)
+// 6. git push origin master
+
+// Menambah fitur baru :
+// 1. git checkout -b Day01_New
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 let name = "Mutia"
+let umur = 25
